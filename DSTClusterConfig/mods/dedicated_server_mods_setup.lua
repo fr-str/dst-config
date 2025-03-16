64886f9283ed56e720d52cc3c4a427bf4b883e22
Franciszek <fr.strzezek@gmail.com>
2025-03-16 19:46:03 +0100

    format

--- DSTClusterConfig/mods/dedicated_server_mods_setup.lua
+++ DSTClusterConfig/mods/dedicated_server_mods_setup.lua
@@ -1,88 +1,74 @@
---There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
-
---ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
---The Workshop id can be found at the end of the url to the mod's Workshop page.
---Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
---ServerModSetup("350811795")
-
---ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
---The Workshop id can be found at the end of the url to the collection's Workshop page.
---Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
--- API
-ServerModSetup("1378549454")
--- Auto Miner hats
-ServerModSetup("3017907311")
 -- Bats Begone
 ServerModSetup("1101769273")
--- Backpack slot
-ServerModSetup("1101769273")
--- Campfire respawn
-ServerModSetup("569043634")
--- Cluster Plant. Reborn
-ServerModSetup("569043634")
--- Craftable Gears For DST
-ServerModSetup("353697884")
--- Customize Everything
-ServerModSetup("2583221769")
--- Deluxe Cooking Pot - Updated
-ServerModSetup("3064846414")
--- Enhanced Eye mask and Shield of Terror
-ServerModSetup("2722198225")
--- Extended Indicators WIP
-ServerModSetup("358749986")
--- Global Positions
-ServerModSetup("378160973")
--- Hambat can hammer
-ServerModSetup("485014178")
--- Increased Stack size
-ServerModSetup("374550642")
--- Moving Box (Updated)
-ServerModSetup("2353205177")
--- No Fire Spread
-ServerModSetup("502726294")
--- no grassgekko
-ServerModSetup("797304209")
--- No Thermal Stone Durability
-ServerModSetup("466732225")
--- Quick Pick
-ServerModSetup("501385076")
--- Repair Antlion Sinkholes
-ServerModSetup("2156905460")
--- Simple Storage
-ServerModSetup("3383078008")
--- Sleeping Buff
-ServerModSetup("2991592240")
+-- Epic Healthbar
+ServerModSetup("1185229307")
+-- Cluster Plant. Reborn!
+ServerModSetup("1337321458")
+-- Basements
+ServerModSetup("1349799880")
+-- [API] Gem Core
+ServerModSetup("1378549454")
 -- Smart Minisign
 ServerModSetup("1595631294")
--- [DST] Storeroom
-ServerModSetup("623749604")
--- Unbreakable Sewable Items
-ServerModSetup("2986162476")
--- Wall Gates [DST]
-ServerModSetup("357875628")
--- Wickerbottom horticulture book improved
-ServerModSetup("2864058489")
--- Wormhole Marks [DST]
-ServerModSetup("362175979")
--- Farm Plant Needs
-ServerModSetup("2326208689")
--- Friendly Fruit Fly QoL
-ServerModSetup("2396207679")
--- Unbreakable WX-78 Modules/Circuits
--- ServerModSetup("2804492445")
--- Build It From Chest
-ServerModSetup("2815192618")
--- Moving Box
-ServerModSetup("2353205177")
 -- Auto Stack and Pick Up
 ServerModSetup("1803285852")
+-- Repair Antlion Sinkholes
+ServerModSetup("2156905460")
+-- Insight (Show Me+)
+ServerModSetup("2189004162")
 -- Spoilage Tweaks: Seeds
 ServerModSetup("2191732916")
--- -- quick pick
--- ServerModSetup("2791452067")
--- Epic health
-ServerModSetup("1185229307")
--- insight
-ServerModSetup("2189004162")
--- potato sack
+-- Moving Box (Updated)
+ServerModSetup("2353205177")
+-- Friendly Fruit Fly QoL
+ServerModSetup("2396207679")
+-- Customize Everything
+ServerModSetup("2583221769")
+-- Enhanced Eye mask and Shield of Terror
+ServerModSetup("2722198225")
+-- Wickerbottom horticulture book improved
+ServerModSetup("2864058489")
+-- Better Potato Sack
 ServerModSetup("2864478455")
+-- Unbreakable Sewable Items
+ServerModSetup("2986162476")
+-- Sleeping Buff
+ServerModSetup("2991592240")
+-- Auto Miner Hats(智能矿工帽/智能礦工帽)
+ServerModSetup("3017907311")
+-- Backpack Slot
+ServerModSetup("3040893393")
+-- Deluxe Cooking Pot - Updated
+ServerModSetup("3064846414")
+-- Simple Storage
+ServerModSetup("3383078008")
+-- Craftable Gears For DST
+ServerModSetup("353697884")
+-- Wall Gates [DST]
+ServerModSetup("357875628")
+-- Extended Indicators WIP
+ServerModSetup("358749986")
+-- Bee Nice
+ServerModSetup("361994110")
+-- Wormhole Marks [DST]
+ServerModSetup("362175979")
+-- Increased Stack size
+ServerModSetup("374550642")
+-- Global Positions
+ServerModSetup("378160973")
+-- No Thermal Stone Durability
+ServerModSetup("466732225")
+-- Hambat Can Hammer
+ServerModSetup("485014178")
+-- Quick Pick
+ServerModSetup("501385076")
+-- No Fire Spread
+ServerModSetup("502726294")
+-- Campfire Respawn
+ServerModSetup("569043634")
+-- [DST] Storeroom
+ServerModSetup("623749604")
+-- no grassgekko 禁用草蜥蜴
+ServerModSetup("797304209")
+-- Lazy Crafting
+ServerModSetup("928938995")
