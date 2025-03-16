711e6daf8605702438b4b18d44effce8e0d3b84f
Franciszek <fr.strzezek@gmail.com>
2025-03-16 18:05:44 +0100

    dupa

--- DSTClusterConfig/mods/dedicated_server_mods_setup.lua
+++ DSTClusterConfig/mods/dedicated_server_mods_setup.lua
@@ -1,49 +1,43 @@
 --There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
 
 --ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
-	--The Workshop id can be found at the end of the url to the mod's Workshop page.
-	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
-	--ServerModSetup("350811795")
+--The Workshop id can be found at the end of the url to the mod's Workshop page.
+--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
+--ServerModSetup("350811795")
 
-	--ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
-	--The Workshop id can be found at the end of the url to the collection's Workshop page.
-	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
-	-- ServerModCollectionSetup("2559688916")
-	-- Bats Begone
-	-- ServerModSetup("1101769273")
-	-- Farm Plant Needs
-	-- ServerModSetup("2326208689")
-	-- Friendly Fruit Fly QoL
-	-- ServerModSetup("2396207679")
-	-- Unbreakable WX-78 Modules/Circuits
-	-- ServerModSetup("2804492445")
-	-- Build It From Chest
-	-- ServerModSetup("2815192618")
-	-- Moving Box
-	-- ServerModSetup("2353205177")
-	-- Auto Stack and Pick Up
-	-- ServerModSetup("1803285852")
-	-- Spoilage Tweaks: Seeds
-	-- ServerModSetup("2191732916")
-	-- quick pick
-	-- ServerModSetup("2791452067")
-	-- better grden book
-	-- ServerModSetup("2864058489")
-	-- stack size
-	-- ServerModSetup("2709425256")
-	-- loot pump
-	-- ServerModSetup("2208128427")
-	-- seed o matic
-	-- ServerModSetup("2675139991")
-	-- vortox soul
-	-- ServerModSetup("1907835622")
-	-- hp
-	-- ServerModSetup("1185229307")
-	-- insight
-	-- ServerModSetup("2189004162")
-	-- potato sack
-	-- ServerModSetup("2864478455")
-	-- ham hammer
-	-- ServerModSetup("485014178")
-	-- maxwell better
-	-- ServerModSetup("1868560353")
+--ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
+--The Workshop id can be found at the end of the url to the collection's Workshop page.
+--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
+ServerModCollectionSetup("2559688916")
+-- Bats Begone
+ServerModSetup("1101769273")
+-- Farm Plant Needs
+ServerModSetup("2326208689")
+-- Friendly Fruit Fly QoL
+ServerModSetup("2396207679")
+-- Unbreakable WX-78 Modules/Circuits
+-- ServerModSetup("2804492445")
+-- Build It From Chest
+ServerModSetup("2815192618")
+-- Moving Box
+ServerModSetup("2353205177")
+-- Auto Stack and Pick Up
+ServerModSetup("1803285852")
+-- Spoilage Tweaks: Seeds
+ServerModSetup("2191732916")
+-- quick pick
+ServerModSetup("2791452067")
+-- better grden book
+ServerModSetup("2864058489")
+-- stack size
+ServerModSetup("2709425256")
+-- Epic health
+ServerModSetup("1185229307")
+-- insight
+ServerModSetup("2189004162")
+-- potato sack
+ServerModSetup("2864478455")
+-- ham hammer
+ServerModSetup("485014178")
+-- simple sotrage
+ServerModSetup("3383078008")
