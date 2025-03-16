4aa1187112a048b74d8a46d9a1344e89575de5f8
Franciszek <fr.strzezek@gmail.com>
2025-03-16 19:22:51 +0100

    upda

--- DSTClusterConfig/mods/dedicated_server_mods_setup.lua
+++ DSTClusterConfig/mods/dedicated_server_mods_setup.lua
@@ -8,9 +8,62 @@
 --ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
 --The Workshop id can be found at the end of the url to the collection's Workshop page.
 --Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
-ServerModCollectionSetup("2559688916")
+-- API
+ServerModSetup("1378549454")
+-- Auto Miner hats
+ServerModSetup("3017907311")
 -- Bats Begone
 ServerModSetup("1101769273")
+-- Backpack slot
+ServerModSetup("1101769273")
+-- Campfire respawn
+ServerModSetup("569043634")
+-- Cluster Plant. Reborn
+ServerModSetup("569043634")
+-- Craftable Gears For DST
+ServerModSetup("353697884")
+-- Customize Everything
+ServerModSetup("2583221769")
+-- Deluxe Cooking Pot - Updated
+ServerModSetup("3064846414")
+-- Enhanced Eye mask and Shield of Terror
+ServerModSetup("2722198225")
+-- Extended Indicators WIP
+ServerModSetup("358749986")
+-- Global Positions
+ServerModSetup("378160973")
+-- Hambat can hammer
+ServerModSetup("485014178")
+-- Increased Stack size
+ServerModSetup("374550642")
+-- Moving Box (Updated)
+ServerModSetup("2353205177")
+-- No Fire Spread
+ServerModSetup("502726294")
+-- no grassgekko
+ServerModSetup("797304209")
+-- No Thermal Stone Durability
+ServerModSetup("466732225")
+-- Quick Pick
+ServerModSetup("501385076")
+-- Repair Antlion Sinkholes
+ServerModSetup("2156905460")
+-- Simple Storage
+ServerModSetup("3383078008")
+-- Sleeping Buff
+ServerModSetup("2991592240")
+-- Smart Minisign
+ServerModSetup("1595631294")
+-- [DST] Storeroom
+ServerModSetup("623749604")
+-- Unbreakable Sewable Items
+ServerModSetup("2986162476")
+-- Wall Gates [DST]
+ServerModSetup("357875628")
+-- Wickerbottom horticulture book improved
+ServerModSetup("2864058489")
+-- Wormhole Marks [DST]
+ServerModSetup("362175979")
 -- Farm Plant Needs
 ServerModSetup("2326208689")
 -- Friendly Fruit Fly QoL
@@ -25,19 +78,11 @@ ServerModSetup("2353205177")
 ServerModSetup("1803285852")
 -- Spoilage Tweaks: Seeds
 ServerModSetup("2191732916")
--- quick pick
-ServerModSetup("2791452067")
--- better grden book
-ServerModSetup("2864058489")
--- stack size
-ServerModSetup("2709425256")
+-- -- quick pick
+-- ServerModSetup("2791452067")
 -- Epic health
 ServerModSetup("1185229307")
 -- insight
 ServerModSetup("2189004162")
 -- potato sack
 ServerModSetup("2864478455")
--- ham hammer
-ServerModSetup("485014178")
--- simple sotrage
-ServerModSetup("3383078008")
